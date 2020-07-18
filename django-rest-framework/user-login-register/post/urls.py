42ee2a376abb5a12f524c758ca19f3e088922629
sixfwa <farhanaliw6@gmail.com>
2020-07-19 00:36:10 +0100

    only auth user can perform CRUD on posts

--- django-rest-framework/user-login-register/post/urls.py
+++ django-rest-framework/user-login-register/post/urls.py
@@ -4,7 +4,9 @@ from rest_framework.routers import DefaultRouter
 from post import views
 
 router = DefaultRouter()
+router.register("me", views.MyPostViewSet)
 router.register("", views.PostViewSet)
+
 
 app_name = "post"
 
--- django-rest-framework/user-login-register/post/views.py
+++ django-rest-framework/user-login-register/post/views.py
@@ -1,9 +1,30 @@
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 
 from post.models import Post
 from post.serializers import PostSerializer
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
+
+class MyPostViewSet(viewsets.ModelViewSet):
+    """Manage the posts by the user"""
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+
+        return self.queryset.filter(user=self.request.user)
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -12,17 +33,7 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
-
-    def get_queryset(self):
-
-        if self.request.user.is_staff:
-            return self.queryset
-        else:
-
-            queryset = self.queryset
-
-            return queryset.filter(user=self.request.user)
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
